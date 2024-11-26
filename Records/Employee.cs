0bdb99b532ea0fdced6bb293898932d3283189af
Adrian <marcelonads@gmail.com>
2024-11-26 17:23:17 +0800

    development: Refactor Employee.cs

--- Records/Employee.cs
+++ Records/Employee.cs
@@ -1,17 +1,19 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace EmployeeManagementConsoleApp.Records;
 
 public record Employee (int? ID, string Name, string Role, string Department, decimal Salary)
 {
-    public decimal ComputeTaxDeduction() => 0;
-    public decimal ComputeAnnualSalary() => Salary * 12;
+
+    public decimal ComputeAnnualIncome() => this.Salary * 12;
 
     public string GetDetails() => $@"
 ID: {ID}
 Name: {Name}
 Role: {Role}
 Department: {Department}
-Salary: {Salary}
-Annual Salary: {this.ComputeAnnualSalary()}
+Monthly Salary: {Salary:C}
+Annual Salary: {this.ComputeAnnualIncome():C} 
 ";
 }
 
